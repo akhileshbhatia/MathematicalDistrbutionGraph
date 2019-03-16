f85bde10b2ea33d75cfb539e367229c0d32f3cfe
Akhilesh Bhatia <akhileshbhatia93@gmail.com>
2019-03-16 17:47:37 +0000

    writing sequence and plot to file

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def getDictWithProbablity(sequence):
         else:
             dict[key] = 1
     
-    #calculate probablity
+    #calculate probability
     for key,value in dict.items():
         dict[key] = value/sequenceLength 
     return dict
@@ -54,8 +54,21 @@ def getCumSum(sequence):
             cumSum[i] = round(cumSum[i-1] + sequence[i],2)
     return cumSum
 
+def writeToFile(obj1,obj2=None):
+    if(obj2 == None):
+        print("Writing sequence to file...")
+        with open("sequence.txt", "w") as f:
+            for item in obj1:
+                f.write(str(item) + "\n")
+    else:
+        print("Writing cdf data to file...")
+        with open("cdf.txt","w") as f:
+            for i in range(len(obj1)):
+                f.write(str(obj1[i]) + " " + str(obj2[i]) + "\n")
+
 def generateCompleteDistributionData():
 # filename distributionType seed numOfIterations par1(optional) par2(optional)
+    print("Generating sequence...")
     if sys.argv[1].lower() == "geometric":
         sequence = generateDistributionSequence(int(sys.argv[2]),"Geometric", int(sys.argv[3]))
     if sys.argv[1].lower() == "exponential":
@@ -63,22 +76,29 @@ def generateCompleteDistributionData():
     if sys.argv[1].lower() == "power":
         sequence = generateDistributionSequence(int(sys.argv[2]),"Power", int(sys.argv[3]), float(sys.argv[4]),float(sys.argv[5]))
     sequence.sort()
-#     print(sequence)
-    dict = getDictWithProbablity(sequence)
-#     print(dict)
-    cumSum = getCumSum(list(dict.values()))
-#     print(cumSum)
-    return dict.keys(), cumSum
+    writeToFile(sequence)
+    
+    print("Generating cdf data...")
+    dictProb = getDictWithProbablity(sequence)
+    cumSum = getCumSum(list(dictProb.values()))
+    writeToFile(list(dictProb.keys()),cumSum)
+    
+    return dictProb.keys(), cumSum
 
-            
+
+f = plt.figure()
 xData, yData = generateCompleteDistributionData()
-# print(xData)
-# print(yData)
+print("Generating plot...")
 if sys.argv[1].lower() == "geometric":
     plt.step(xData,yData)
 else:
     plt.plot(xData,yData)
-plt.show()
+# plt.show()
+print("Saving plot to file...")
+
+f.savefig("plot.pdf",bbox_inches="tight")
+print("All files generated. Please check the current folder.")
+
 
     
 
